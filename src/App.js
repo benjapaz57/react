474dcf21fa6cd3ccb29e10830a0ae30de241b38c
benjapaz57 <89224883+benjapaz57@users.noreply.github.com>
2022-02-17 19:31:16 -0300

    pre entrega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Navbar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from './components/ItemDetailContainer'
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <>
-        <Navbar />
-        <ItemListContainer greeting={'Somos la mejor tienda'} />
-    </>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element= {<ItemListContainer />} />
+      <Route path='/category/:idCategory' element={<ItemDetailContainer />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './CartWidget';
 import CartWidget from './CartWidget';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-secondary bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">SDE-Wallets</a>
+                    <Link to='/'>SDE-Wallets</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="#">Productos</a>
+                                <Link to='/category/'>Productos</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Formas de Pago</a>
+                                <Link to='/category/'>Formas de Pago</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Nosotros</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contacto</a>
+                                <Link to='/category/'>Contacto</Link>
                             </li>
                             <CartWidget />
                         </ul>
